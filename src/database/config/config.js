59ff604ae8743a22a395c10d4e034e75edd2e0fa
MargaritaSyd <margarita.syddall@gmail.com>
2022-03-28 13:13:03 -0300

    config database

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,24 +1,28 @@
-{
+
+
+module.exports = {
   "development": {
     "username": "root",
     "password": "darioj8121",
     "database": "pdfdb",
     "host": "mysql.revistapalermo.com.ar",
     "dialect": "mysql",
-    "port": "3306"
+    "port": 3306
   },
   "test": {
     "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "password": "darioj8121",
+    "database": "pdfdb",
+    "host": "mysql.revistapalermo.com.ar",
+    "dialect": "mysql",
+    "port": 3306
   },
   "production": {
     "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "password": "darioj8121",
+    "database": "pdfdb",
+    "host": "mysql.revistapalermo.com.ar",
+    "dialect": "mysql",
+    "port": 3306
   }
 }
