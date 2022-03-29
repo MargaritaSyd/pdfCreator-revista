48da7bf4d03ff154a24a0d8ab1228b9532104594
MargaritaSyd <margarita.syddall@gmail.com>
2022-03-29 13:04:06 -0300

    criador_id = 0

--- src/controller/indexController.js
+++ src/controller/indexController.js
@@ -15,36 +15,27 @@ const readFile = utils.promisify(fs.readFile)
 
 let indexController = {
     index: function(req,res){
-        /*
-        db.ejemplares.findByPk(req.params.id)
-        .then(function(ejemplar){
-            res.render("index" , {ejemplar})
-        })
-        */
-/*
-            let unEjemplar = db.ejemplares.findByPk(req.params.id)
-         //   let elCriador = unEjemplar.dataValues.criador_id
-            let criador = db.criadores.findByPk(elCriador)
-            Promise.all([unEjemplar , criador])
-            .then(function([unEjemplar , criador]){
-                console.log(criador)
-                //res.render("index" , {unEjemplar , criadores})
-            })
-            .catch(function(err){
-                console.log(err)
-            })
-
-
-       */
+        
 
             db.ejemplares.findByPk(req.params.id)
             .then(function(ejemplar){
                 let elCriador = ejemplar.dataValues.criador_id  
+                 db.criadores.findByPk(elCriador)
+                
 
-                db.criadores.findByPk(elCriador)
-            
             .then(function(criador){
-                let criadorX = criador.dataValues
+                if(criador.id != 0){
+                    let criadorX = criador.dataValues
+                 
+                } else {
+                    let criadorX = {
+                        id: 0,
+                        propietario: "N/N",
+                        haras: "N/N",
+                        banner: "N/N"
+                    }
+                }
+
                 let ejemplarX = ejemplar.dataValues
                 res.render("htmlToPdf" , {criadorX , ejemplarX})
            })
@@ -57,6 +48,86 @@ let indexController = {
        
         
     },
+
+    pdfCreator: function(req,res){
+/*
+    db.ejemplares.findByPk(req.params.id)
+            .then(function(ejemplar){
+                let elCriador = ejemplar.dataValues.criador_id  
+
+                db.criadores.findByPk(elCriador)
+            
+            .then(function(criador){
+                let criadorX = criador.dataValues
+                let ejemplarX = ejemplar.dataValues
+                res.render("htmlToPdf" , {criadorX , ejemplarX})
+          
+*/
+        
+
+        db.ejemplares.findByPk(req.params.id)
+        .then(function(ejemplar){
+            let elCriador = ejemplar.dataValues.criador_id  
+
+            db.criadores.findByPk(elCriador)
+        
+        .then(function(criador){
+            let criadorX = criador.dataValues
+            let ejemplarX = ejemplar.dataValues
+            
+       async function getTemplateHtml() {
+           console.log("Loading template file in memory")
+           try {
+           const invoicePath = path.resolve("./views/index.ejs");
+           return await readFile(invoicePath, 'utf8');
+           } catch (err) {
+           //return Promise.reject("Could not load html template");
+           console.log(err)
+           }
+           }
+           async function generatePdf() {
+           let data = {ejemplarX , criadorX};
+           getTemplateHtml().then(async (res) => {
+           // Now we have the html code of our template in res object
+           // you can check by logging it on console
+           // console.log(res)
+           console.log("Compiing the template with handlebars")
+           const template = hb.compile(res, { strict: true });
+           // we have compile our code with handlebars
+           const result = template(data);
+           // We can use this to add dyamic data to our handlebas template at run time from database or API as per need. you can read the official doc to learn more https://handlebarsjs.com/
+           const html = result;
+           // we are using headless mode
+           const browser = await puppeteer.launch();
+           const page = await browser.newPage()
+           // We set the page content as the generated html by handlebars
+           await page.setContent(html)
+           // We use pdf function to generate the pdf in the same folder as this file.
+           await page.pdf({ 
+               path: 'invoice.pdf', 
+               width: "190mm",
+               height: "270mm",
+               // format: 'A4' 
+           })
+           await browser.close();
+           console.log("PDF Generated")
+           
+           }).catch(err => {
+           console.error(err)
+           });
+           }
+           generatePdf();
+       //res.render("index");
+   })
+})
+.then(function(){
+    res.send("ok")
+})
+   },
+   error: function(req,res) {
+       res.send('error');
+   }
+    /*
     pdfCreator: function(req,res){
         
          db.ejemplares.findByPk(req.params.id)
@@ -111,6 +182,7 @@ let indexController = {
     error: function(req,res) {
         res.send('error');
     }
+    */
 }
 
 module.exports = indexController;
