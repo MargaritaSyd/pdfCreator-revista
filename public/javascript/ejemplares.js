85d2ad54f2b7143f41f5afa75f9ba04000407d2d
MargaritaSyd <margarita.syddall@gmail.com>
2022-04-22 16:13:08 -0300

    .

--- public/javascript/ejemplares.js
+++ public/javascript/ejemplares.js
@@ -2,12 +2,14 @@
 window.addEventListener("load" , function(){
   let button = document.getElementById('boton')
   let id = document.getElementById('id').value
+  let nombreDelEjemplar = document.getElementById('nombreDelEjemplar')
 alert(id)
 let url = 'http://localhost:3002/api_ejemplar/'+ id;
+//let ejemplar
 
 
 alert('ok')
-button.addEventListener('click' , function(){
+//button.addEventListener('click' , function(){
 
 
 fetch(url)
@@ -15,10 +17,20 @@ fetch(url)
     return r.json();
 })
 .then(function(data){
-    alert('capa mundial')
+  
+    //ejemplar = data.nombre
+    let ladata = JSON.stringify(data)
+    let ejemplar = ladata.data
+    let stringEjemplar = JSON.stringify(ejemplar)
+    //let nombre = ladata.data.nombre
+    alert(ladata)
 })
 .catch(function(err){
   console.log(err);
 });
+
+//nombreDelEjemplar.innerHTML = data.nombre
+
+
 })
-})
+//})
