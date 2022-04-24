132ba686308deb49958a8be34bbc37166e98e147
MargaritaSyd <margarita.syddall@gmail.com>
2022-04-24 14:33:57 -0300

    hijos de la primera madre ok

--- public/javascript/madres.js
+++ public/javascript/madres.js
@@ -1,7 +1,9 @@
 window.addEventListener('load' , function(){
 let id = document.getElementById('id').value
 let urlUnEjemplar = 'http://localhost:3002/api_ejemplar/';
+let urlHijos = 'http://localhost:3002/api_hijos/';
 
+let idMadre
 //TRAIGO INFO DEL EJEMPLAR PARA TENER INFO DE SU 1 MADRE
 fetch(urlUnEjemplar + id)
 .then(function(r){
@@ -14,5 +16,19 @@ fetch(urlUnEjemplar + id)
 })
 .catch(function(err){
     alert(err + 'ejemplar')
+})
+//TRAIGO INFO DE HIJOS DE LA PRIMERA MADRE
+.then(function(){
+    fetch(urlHijos + idMadre)
+    .then(function(r){
+        return r.json();
+    })
+    .then(function(data){
+        alert('ok la primera madre')
+    })
+    .catch(function(err){
+        alert(err + 'hijos primera madre')
+    })
+
 })
 })
