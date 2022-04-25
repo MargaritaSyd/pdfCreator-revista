b6c28a99c1a7313d6d512b651bddb3c884d7f960
MargaritaSyd <margarita.syddall@gmail.com>
2022-04-25 08:25:44 -0300

    creando objeto por hijo

--- public/javascript/madres.js
+++ public/javascript/madres.js
@@ -5,7 +5,12 @@ let urlHijos = 'http://localhost:3002/api_hijos/';
 let urlResultadosHijo = 'http://localhost:3002/api_resultados_hijos/'
 
 let idMadre
-let arrayHijos = []
+let arrayHijos = [];
+idCarreras = [];
+let nombreI
+               
+
+    
 //TRAIGO INFO DEL EJEMPLAR PARA TENER INFO DE SU 1 MADRE
 fetch(urlUnEjemplar + id)
 .then(function(r){
@@ -29,29 +34,79 @@ fetch(urlUnEjemplar + id)
         let datadata = data.data
         for(i of datadata){
             let idI = i.id
+            let nombreHijo = i.nombre
             fetch(urlResultadosHijo + idI)
             .then(function(r){
                 return r.json();
             })
+            
             .then(function(data){
-                //alert('ok el resultado')
-              arrayHijos.push(data)
+ 
+let arrayLlego1 = [];
+let arrayLlego2 = [];
+let arrayLlego3 = [];
+
+             for(i of data.data){
+                 if(i.llego_numero == 1){
+                    arrayLlego1.push(i.carrera_id)
+                 } else if (i.llego_numero == 2){
+                     arrayLlego2.push(i.carrera_id)
+                 } else {
+                     arrayLlego3.push(i.carrera_id)
+                 }
+               
+ 
+             }
+             const resultadoHijo = new Object();
+             resultadoHijo.id = idI;
+             resultadoHijo.nombre = nombreHijo
+             //resultadoHijo.hipodromo = 50;
+             resultadoHijo.primero = arrayLlego1;
+             resultadoHijo.segundo = arrayLlego2;
+             resultadoHijo.tercero = arrayLlego3;
+             let res = JSON.stringify(resultadoHijo)
+             alert(res)
             })  
             .catch(function(err){
                 alert(err + 'resultadosHijos')
             })
+            /*
             .then(function(){
-                let resultados = JSON.stringify(arrayHijos)
-                alert(resultados)
+                const resultadoHijo = new Object();
+                resultadoHijo.id = idI;
+                resultadoHijo.nombre = nombreHijo
+                //resultadoHijo.hipodromo = 50;
+                resultadoHijo.primero = arrayLlego1;
+                resultadoHijo.segundo = arrayLlego2;
+                resultadoHijo.tercero = arrayLlego3;
+
+            })
+            */
+            .then(function(){
+                alert(resultadoHijo)
             })
         }
+       
     })
+   
     .catch(function(err){
         alert(err + 'hijos primera madre')
     })
-    .then(function(){
-        alert(arrayHijos)
-    })
+   
 
 })
+/*
+Promise.all([arrayLlego1, arrayLlego2, arrayLlego3])
+.then(function(){
+    alert(arrayLlego1 + 'este es el nombre')
 })
+*/
+})
+
+/*
+                const resultadoHijo = new Object();
+                resultadoHijo.id = "John";
+                resultadoHijo.nombre = "Doe";
+                resultadoHijo.hipodromo = 50;
+                resultadoHijo.llego_numero = "blue";
+                */
